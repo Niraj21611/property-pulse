bd7a110e35dab896578ab4e5c17b06f1e14063f1
Niraj Kshirsagar <nirajkshirsagar72@gmail.com>
2024-08-25 00:20:37 +0530

    Added new page showing saved(bookmarked) properties by user

--- app/api/bookmark/route.js
+++ app/api/bookmark/route.js
@@ -1,8 +1,35 @@
 import connectDB from "@/config/database";
+import Property from "@/models/Property";
 import User from "@/models/User";
 import { useCurrentSession } from "@/utils/useSession";
 import { NextResponse } from "next/server";
 
+//GET - /api/bookmark
+export const GET = async (request) => {
+  try {
+    await connectDB();
+
+    const session = await useCurrentSession();
+    if (!session || !session.userId) {
+      return NextResponse.json("User Id is required", { status: 401 });
+    }
+
+    const { userId } = session;
+
+    const user = await User.findOne({ _id: userId });
+
+    const bookmarks = await Property.find({ _id: { $in: user.bookmarks } });
+    return NextResponse.json(bookmarks, { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { error: "Something went wrong" },
+      { status: 500 }
+    );
+  }
+};
+
+// POST - /api/bookmark
 export const POST = async (request) => {
   try {
     await connectDB();
---
+++ app/properties/saved/page.jsx
@@ -0,0 +1,60 @@
+"use client";
+
+import PropertyCard from "@/components/PropertyCard";
+import Spinner from "@/components/Spinner";
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+
+function SavedProperties() {
+  const [savedProperties, setSavedProperties] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const { data: session } = useSession();
+  const userId = session?.user?.id;
+
+  useEffect(() => {
+    async function fetchSavedProperties() {
+      try {
+        const res = await fetch(`/api/bookmark`);
+        if (res.status === 200) {
+          const data = await res.json();
+          setSavedProperties(data);
+        } else {
+          console.log(res.statusText);
+          toast.error("Failed to fetch saved properties");
+        }
+      } catch (error) {
+        console.log(error);
+        toast.error("Failed to fetch saved properties");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchSavedProperties();
+  }, []);
+
+  console.log(savedProperties);
+
+  if (isLoading) return <Spinner loading={isLoading} />;
+
+  return (
+    <section className="px-4 py-6">
+      <div className="container-xl lg:container m-auto">
+        <h2 className="text-3xl font-bold text-blue-500 mb-6 text-center">
+          Saved Properties
+        </h2>
+        {savedProperties.length === 0 ? (
+          <p>No Saved Properties</p>
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {savedProperties.map((property) => (
+              <PropertyCard property={property} key={property._id} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
+  );
+}
+
+export default SavedProperties;
